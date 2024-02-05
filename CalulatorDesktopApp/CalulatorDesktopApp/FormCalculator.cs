d6e1768870aea0232084e2cfab05096ce6f09a08
[samia <32800475+samiabari@users.noreply.github.com>
2024-02-05 17:36:03 +0800

    Digits Added

--- CalulatorDesktopApp/CalulatorDesktopApp/FormCalculator.cs
+++ CalulatorDesktopApp/CalulatorDesktopApp/FormCalculator.cs
@@ -1,8 +1,8 @@
 namespace CalulatorDesktopApp
 {
-    public partial class Form1 : Form
+    public partial class FormCalculator : Form
     {
-        public Form1()
+        public FormCalculator()
         {
             InitializeComponent();
         }
